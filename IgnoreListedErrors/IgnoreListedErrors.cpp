9bac30ba332aab504fd917a15f3ed1a289191da4
Lauren Prinn <laurenprinn@microsoft.com>
2026-04-13 13:29:02 -0700

    Add project files.

---
+++ IgnoreListedErrors/IgnoreListedErrors.cpp
@@ -0,0 +1,51 @@
+﻿#include "IgnoreListedErrors.h"
+
+using namespace std;
+
+// This works in v140/v141 but fails in v143+ with C2143
+class module {  // 'module' became a keyword in C++20
+	int data;
+};
+
+class FakeCalculator
+{
+public:
+	double add(double a, double b) { return a + b; }
+	double subtract(double a, double b) { return a - b; }
+	double multiply(double a, double b) { return a * b; }
+	double divide(double a, double b)
+	{
+		if (b == 0)
+		{
+			cout << "Error: Division by zero!" << endl;
+			return 0;
+		}
+		return a / b;
+	}
+
+	void displayResult(const string& operation, double result)
+	{
+		cout << "finished" << endl;
+	}
+};
+
+int concept = 5;  // 'concept' is a C++20 keyword
+auto co_await = 10;  // 'co_await' is a C++20 coroutine keyword
+
+
+int main()
+{
+
+
+	FakeCalculator calc;
+	int y = 10;
+	cout << "Hello CMake." << endl;
+	cout << "\nFake Calculator Demo:" << endl;
+	calc.displayResult("10 + 5", calc.add(10, 5));
+	calc.displayResult("10 - 5", calc.subtract(10, 5));
+	calc.displayResult("10 * 5", calc.multiply(10, 5));
+	calc.displayResult("10 / 5", calc.divide(10, 5));
+	calc.displayResult("10 / 0", calc.divide(10, 0));
+
+	return 0;
+}
